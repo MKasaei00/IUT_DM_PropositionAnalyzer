3594883ffc3f48e060464fa492cd2181ac3312c7
Mohammad Kasaei <MohammadKasaei00@yahoo.com>
2019-04-12 08:20:32 +0430

    Basic cli interface designed

---
+++ DiscreteMath_PropositionAnalyzer/InterfaceNotes.cpp
@@ -0,0 +1,21 @@
+/*
+Interface Notes:
+	TruthTable
+	is Tautology
+	is Contradition
+	equivalent
+	inference checking
+	satisfiable or no
+
+
+CLI commands:
+	table	[input file name]
+	total	[input file name]
+	contr	[input file name]
+	satis	[input file name]
+	equva	[input file name (two first lines is read)]
+	infer	[input file name (has n line : n-1 lines premises the last is conclusion)]
+
+	help
+	exit
+*/
